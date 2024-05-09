b6055090c363e8787d7b998a1ae70069a23529e9
quantumcoder1 <sathnil19@gmail.com>
2024-05-09 08:06:06 -0700

    fixed Student.java

--- src/main/java/com/csase/PlannerRunner.java
+++ src/main/java/com/csase/PlannerRunner.java
@@ -5,9 +5,11 @@ public class PlannerRunner {
 
         // Create a new student
         Student jada = new Student();
+        Student nilesh = new Student();
 
         // Gets the student's choice until the student exits
         jada.getChoice();
+        nilesh.getChoice();
 
     }
 }
